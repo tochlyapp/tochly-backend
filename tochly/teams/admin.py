88b7c7860932ad94d1f1980301faa7f7e8671823
Ahmad Ameen <ahmadmameen7@gmail.com>
2023-11-27 18:06:38 +0100

    refactor: remove django-tenants

--- tochly/teams/admin.py
+++ tochly/teams/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django_tenants.admin import TenantAdminMixin
 
 from teams.models import Team
 
 
 @admin.register(Team)
-class TeamAdmin(TenantAdminMixin, admin.ModelAdmin):
+class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'created', 'last_updated')
     list_filter = ('created', 'last_updated')
     search_fields = ('name',)
