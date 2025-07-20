39de879586580c54f89db84b61d3aae08e53bd86
Ahmad Ameen <ahmadmameen7@gmail.com>
2025-07-20 22:46:40 +0100

    fix: settings.py

--- tochly/tochly/settings.py
+++ tochly/tochly/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/backend/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = '/backend/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
