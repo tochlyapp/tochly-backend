f14125736d022b2c635b217dfb722fe03179f53a
Ahmad Ameen <ahmadmameen7@gmail.com>
2025-07-20 19:51:15 +0100

    fix: settings.py

--- tochly/tochly/settings.py
+++ tochly/tochly/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # load env config
 load_dotenv(dotenv_path=BASE_DIR / '../.env')
